f77c3c8d23f951f5f5de4d8dad85420684ff536c
deeptic <deepti.jchandra@gmail.com>
2015-08-31 14:37:23 -0700

    Version 1.0 - Correcting a few errors and rectifying structures
    Version 1.0 - Correcting a few errors and rectifying structures

--- ZTP/dhcpcopy.py
+++ ZTP/dhcpcopy.py
@@ -1,38 +1,115 @@
-#!/usr/bin/env python    
+#!/usr/bin/env python
 
-import pexpect
-import argparse
+global logFile
+global local
+global remotepath
 
-parser = argparse.ArgumentParser(description='Copy DHCP config file to remote server')
-parser.add_argument("--host", dest="hostname", default="",metavar="HOST",help="Specify remote host")
-parser.add_argument("--username", dest="username", metavar="USERNAME",help="Specify the username")
-parser.add_argument("--password", dest="password", metavar="PASSWORD",help="Specify the password")
-args = parser.parse_args()
 
-host = args.hostname
-user = args.username
-var_password = args.password
 logFile = "/Users/deeptic/Desktop/deeptic/Desktop/dhcpconf/plog.txt"
 local = "/Users/deeptic/Desktop/deeptic/Desktop/dhcpconf/dhcpd.conf"
 remotepath = "/home/devops/Desktop"
-remote = user + '@' + host + ':' + remotepath 
-    
-try:
-        var_command = "scp " + local + ' ' + remote
-        
-        
-        var_child = pexpect.spawn(var_command)
-        var_child.logfile= open(logFile, "w")
-        i = var_child.expect(["password:", pexpect.EOF])
 
-        if i==0:                 
-                var_child.sendline(var_password)
-                var_child.expect(pexpect.EOF)
-        elif i==1: 
-                print "Got the key or connection timeout"
-                pass
-                           
 
-except Exception as msg:
-        print "Terminating ... error !"
-        print msg 
+def copyDhcp(host, user, password):
+        import pexpect
+
+        global sshServer
+
+        remote = user + '@' + host + ':' + remotepath
+        sshServer = user + '@' + host
+
+        try:
+                scp_command = "scp " + local + ' ' + remote
+
+                session = pexpect.spawn(scp_command)
+                session.logfile= open(logFile, "w")
+                i = session.expect(["password:", pexpect.EOF])
+
+                if i==0:
+                        session.sendline(password)
+                        print "File Copy Completed !"
+                        session.expect(pexpect.EOF)
+                elif i==1:
+                        print "Got the key or connection timeout"
+                        pass
+
+
+        except Exception as msg:
+                print "Terminating ... Error !"
+                print msg
+
+
+
+
+def runCmds(host, user, password):
+                import pexpect
+
+                global prompt
+                prompt = '.*@.*\$'
+
+                sshNewkey = 'Are you sure you want to continue connecting'
+
+                session=pexpect.spawn("ssh %s" %sshServer)
+
+
+                session.logfile= open(logFile, "w")
+
+                chkKey=session.expect([sshNewkey,'password:',pexpect.EOF,pexpect.TIMEOUT],90) #0 if no key, 1 if has key
+
+                if chkKey==0 :
+
+                                print "\n\t => Server not known - Adding ssh key"
+
+                                session.sendline("yes")
+
+                                chkKey=session.expect([sshNewkey,'Password:',pexpect.EOF])
+
+                if chkKey==1:
+
+                                print "\n\t =>  Server ssh key known - Proceeding with password authentication"
+
+                                session.sendline("%s" %password)
+                                session.expect("%s" %prompt)
+
+                                session.timeout=60
+
+                                commandSet = ['date', 'sudo mv /home/devops/Desktop/dhcpd.conf /etc/dhcp/' ,'date', 'ls -lrt /etc/dhcp/' ,'date', 'sudo service isc-dhcp-server restart', 'date', 'date']
+
+                                for element in commandSet:
+                                        session.sendline("\r")
+                                        session.expect("%s \r" %prompt)
+                                        session.sendline("\r")
+                                        session.sendline("%s \r" %element)
+                                        session.sendline("\r")
+
+
+
+
+
+                elif chkKey==2:
+
+                        print "\n\t => Connection timeout"
+
+                        pass
+
+
+
+def main():
+        import argparse
+
+        parser = argparse.ArgumentParser(description='Copy DHCP config file to remote server')
+        parser.add_argument("--host", dest="hostname", default="",metavar="HOST",help="Specify remote host")
+        parser.add_argument("--username", dest="username", metavar="USERNAME",help="Specify the username")
+        parser.add_argument("--password", dest="password", metavar="PASSWORD",help="Specify the password")
+        args = parser.parse_args()
+
+        host = args.hostname
+        user = args.username
+        password = args.password
+
+        copyDhcp(host, user, password)
+        runCmds(host, user, password)
+
+
+if __name__ == "__main__":
+        main()
